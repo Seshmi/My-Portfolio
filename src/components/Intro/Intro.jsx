197190fe98ec5a60e62c9660427485d2ecbf1bbe
Seshmi <it20036898@my.sliit.lk>
2022-07-28 11:51:51 +0530

    added girl image

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,6 +3,7 @@ import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
+import girl from "../../img/girl.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
@@ -60,7 +61,7 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <img src={girl} alt="" />
         {/* animation */}
         <motion.img
           initial={{ left: "-36%" }}
